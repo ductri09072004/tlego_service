6bb8e5f63e107850d673e8eb45a4d25110ae5073
ThrisRH <144113163+ThrisRH@users.noreply.github.com>
2025-03-16 11:04:59 +0700

    get user by id

--- src/controllers/Customers.controller.js
+++ src/controllers/Customers.controller.js
@@ -1,4 +1,4 @@
-import { database } from "../data/firebaseConfig.js";
+import { admin, database } from "../data/firebaseConfig.js";
 
 // Lấy danh sách tất cả requests từ Firebase
 export const getRequests = async (req, res) => {
@@ -17,4 +17,25 @@ export const getRequests = async (req, res) => {
   }
 };
 
+export const getUserInfo = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const snapshot = await database
+      .ref("customers")
+      .orderByChild("cus_id")
+      .equalTo(userId)
+      .once("value");
 
+    if (!snapshot.exists()) {
+      console.log("❌ Không tìm thấy user với cus_id:", cusId);
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const userData = Object.values(snapshot.val())[0];
+
+    return res.status(200).json(userData);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- src/routes/Customers.route.js
+++ src/routes/Customers.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getRequests} from "../controllers/Customers.controller.js";
+import {
+  getRequests,
+  getUserInfo,
+} from "../controllers/Customers.controller.js";
 
 const router = express.Router();
 
 router.get("/customers", getRequests);
+router.get("/customers/userInfo/:id", getUserInfo);
 
 // Xuất theo chuẩn ES Module
 export default router;
